9c958a988fdafdace910a4f09e9f08d81a715a89
Kayo Céshar Rêgo dias dos Santos <78767807+kayoceshar@users.noreply.github.com>
2022-11-29 21:14:25 -0300

    inicio da revisão

---
+++ anotações.md
@@ -0,0 +1,5 @@
+resumo backend
+
+npm init -y
+tsc --init
+npm i typescript @types/node -D
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import chalk from "chalk";
+
+const message = chalk.green('hello world');
+
+console.log(message);
+
+
+
